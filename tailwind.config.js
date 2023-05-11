b584798fd60074077ec589c25ef264457c657c88
arminamirinasab <arminamirinasab@gmail.com>
2023-05-11 19:55:56 +0430

    Add Font Awesome & Design 404 Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   content: ["./pages/**/*.html"],
   theme: {
-    // colors: {
-    //   primary: '#5c6ac4',
-    //   secondary: '#ecc94b',
-    //   // ...
-    // },
+    colors: {
+      primary: '#5c6ac4',
+      secondary: '#ecc94b',
+      bgcolor: "#f0f0f3",
+      title: "rgb(120, 137, 179)",
+      text: "#85a4c4"
+    },
   },
   plugins: [
     require("tailwindcss"),
