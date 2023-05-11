12d3ddac6e234e1a5e650df34ac52d6f7b0c8d4b
arminamirinasab <arminamirinasab@gmail.com>
2023-05-11 23:14:00 +0430

    Design About Page - Armin Task Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,18 @@ module.exports = {
   content: ["./pages/**/*.html"],
   theme: {
     colors: {
-      primary: '#5c6ac4',
-      secondary: '#ecc94b',
+      primary: "#5c6ac4",
+      secondary: "#ecc94b",
       bgcolor: "#f0f0f3",
       title: "rgb(120, 137, 179)",
-      text: "#85a4c4"
+      text: "#85a4c4",
+      social: "#7889b3"
+    },
+    extend: {
+      backgroundImage: {
+        heroimage: "url(../images/hero.jpg)",
+      },
     },
   },
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ],
-}
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
